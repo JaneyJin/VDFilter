ef3577a0122ce631201978d6ae1d2f6efc244539
Yishen Jin <yishenjin@pop-os.localdomain>
2021-08-03 11:29:55 -0400

    Create VD filter test repository

---
+++ cmake-build-debug/CMakeFiles/VDFilter.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/yishenjin/CLionProjects/VDFilter/Client.cpp" "/home/yishenjin/CLionProjects/VDFilter/cmake-build-debug/CMakeFiles/VDFilter.dir/Client.cpp.o"
+  "/home/yishenjin/CLionProjects/VDFilter/MainVD.cpp" "/home/yishenjin/CLionProjects/VDFilter/cmake-build-debug/CMakeFiles/VDFilter.dir/MainVD.cpp.o"
+  "/home/yishenjin/CLionProjects/VDFilter/main.cpp" "/home/yishenjin/CLionProjects/VDFilter/cmake-build-debug/CMakeFiles/VDFilter.dir/main.cpp.o"
+  "/home/yishenjin/CLionProjects/VDFilter/src/TraCIAPI.cpp" "/home/yishenjin/CLionProjects/VDFilter/cmake-build-debug/CMakeFiles/VDFilter.dir/src/TraCIAPI.cpp.o"
+  "/home/yishenjin/CLionProjects/VDFilter/src/socket.cpp" "/home/yishenjin/CLionProjects/VDFilter/cmake-build-debug/CMakeFiles/VDFilter.dir/src/socket.cpp.o"
+  "/home/yishenjin/CLionProjects/VDFilter/src/storage.cpp" "/home/yishenjin/CLionProjects/VDFilter/cmake-build-debug/CMakeFiles/VDFilter.dir/src/storage.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include "MainVD.h"
+#include "Client.h"
+
+int main() {
+    MainVD vd;
+    Client client;
+    int count = 0;
+    double ue_X = 0, ue_Y = 0, ue_V = 0;
+    double psi = 0;
+    libsumo::TraCIPosition sumo_pos;
+    double sumo_angle;
+
+
+    try {
+        client.connect("localhost", 24000);
+
+        while (client.simulation.getMinExpectedNumber() > 0) {
+
+            vd.VD_filter(sumo_pos.x, sumo_pos.y, sumo_angle,&psi, &ue_X, &ue_Y, &ue_V);
+
+
+            if(count % 10 == 0){
+                client.simulationStep();
+
+                count++;
+
+                sumo_pos = client.vehicle.getPosition("car0");
+                sumo_angle = client.vehicle.getAngle("car0");
+                vd.VD_filter(sumo_pos.x, sumo_pos.y, sumo_angle,&psi, &ue_X, &ue_Y, &ue_V);
+
+            }
+
+        }
+    }catch (tcpip::SocketException& e) {
+        std::cout << "#Error while connecting: " << e.what();
+    }
+
+
+
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
